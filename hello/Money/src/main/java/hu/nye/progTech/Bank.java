c4be71136abee60011744c99cc06c0b408902ebd
Carlos Albino Jr <61842021+CarlosAlbino34@users.noreply.github.com>
2023-12-03 21:25:45 +0100

    Add files via upload

---
+++ hello/Money/src/main/java/hu/nye/progTech/Bank.java
@@ -0,0 +1,9 @@
+package hu.nye.progTech;
+
+import java.util.Currency;
+
+public class Bank {
+    Money convertTo(Money moneyToConvert, Currency toCurrency) {
+        return null;
+    }
+}
---
+++ hello/untitled/src/main/java/hu/nye/progetch/helloworld/Main.java
@@ -0,0 +1,7 @@
+package hu.nye.progetch.helloworld;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
